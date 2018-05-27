8629fb327b421c9c206942fc6866269296de066a
Martin Försterling <martinleanderfoersterling@gmail.com>
2018-05-27 17:01:00 +0200

    fixed validation

--- src/main/java/mafoe/jpa21/model/Country.java
+++ src/main/java/mafoe/jpa21/model/Country.java
@@ -1,12 +1,12 @@
 package mafoe.jpa21.model;
 
-import com.sun.istack.internal.NotNull;
 import mafoe.jpa21.converters.TimeZoneConverter;
 
 import javax.persistence.Column;
 import javax.persistence.Convert;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Locale;
